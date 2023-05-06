57c210396eb4849f3cdd7cde4e99817ee335dcad
Jrojal <jimmy.rojas.435@gmail.com>
2023-05-06 19:03:47 -0400

    Agrega de campos

--- src/app/registro/registro.page.ts
+++ src/app/registro/registro.page.ts
@@ -10,7 +10,16 @@ import { Router } from '@angular/router';
 export class RegistroPage implements OnInit {
   //se crea la variable select tabs
   selectTabs = 'recent';
-  
+  //Funcion checkbox
+  checkbox1 = false;
+  checkbox2 = false;
+  onClickCheckbox1() {
+    this.checkbox2 = false;
+  }
+
+  onClickCheckbox2() {
+    this.checkbox1 = false;
+  }
   constructor() { 
 
   }
